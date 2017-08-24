0c2696698859e926bfedb117edef083ae648c8d3
olegfedoliak <oleg.fedoliak@gmail.com>
2017-08-25 00:47:05 +0300

    Added abstarct classes

--- src/Commands/GenerateDomain.php
+++ src/Commands/GenerateDomain.php
@@ -31,6 +31,14 @@ class GenerateDomain extends Command
     public $domainRepositoryTest = __DIR__ . '/../../resources/Tests/TemplateRepositoryTest';
     public $domainServiceTest    = __DIR__ . '/../../resources/Tests/TemplateServiceTest';
 
+    public $abstractEloquentRepositoryInterface =
+        __DIR__ . '/../../resources/Infrastructures/Contracts/EloquentRepositoryInterface';
+    public $abstractEloquentRepository = __DIR__ . '/../../resources/Infrastructures/EloquentRepositoryAbstract';
+    public $abstractEloquent = __DIR__ . '/../../resources/Infrastructures/EloquentAbstract';
+    public $abstractRepository = __DIR__ . '/../../resources/Domains/RepositoryAbstract';
+    public $abstractService = __DIR__ . '/../../resources/Domains/ServiceAbstract';
+    public $abstractBaseEntityInterface = __DIR__ . '/../../resources/Domains/Contracts/BaseEntityInterface';
+
     public $binding    = __DIR__ . '/../../resources/bind';
 
     /**
@@ -69,6 +77,8 @@ class GenerateDomain extends Command
                         {--domain-path=Domains : Domain directory. Default app/Domains}
                         {--infrastructure-path=Infrastructures : Infrastructure directory. Default app/Infrastructures}
                         {--test-path=Tests/Domains : Tests path}
+                        {--without-abstracts : Allows to skip copying abstract classes}
+                        {--forced : Allows to override existing domains}
                         ';
 
     /**
@@ -111,6 +121,11 @@ class GenerateDomain extends Command
             $this->directory = $this->getProvidedName();
         }
 
+        if (!$this->option('forced') and
+            file_exists($this->getDomainPath("/{$this->name}/Contracts/{$this->name}Interface.php"))) {
+            return $this->comment("Domain already exists. Use --forced to override");
+        }
+
         //Get DB fields
         $this->fields = $this->parseDbTable($this->table);
 
@@ -129,7 +144,7 @@ class GenerateDomain extends Command
         //Copy binding
         $this->copyBinding($this->name);
 
-        echo "\033[32m Domain $this->name is created \033[0m \n";
+        $this->info("Domain $this->name is created");
     }
 
     /**
@@ -138,35 +153,94 @@ class GenerateDomain extends Command
      */
     public function createDirectories()
     {
-        if (!File::exists($this->getDomainPath())) {
+        if (!file_exists($this->getDomainPath())) {
             File::makeDirectory($this->getDomainPath());
         }
 
-        if (!File::exists($this->getInfrastructurePath())) {
+        if (!file_exists($this->getInfrastructurePath())) {
             File::makeDirectory($this->getInfrastructurePath());
         }
 
-        if (!File::exists($this->getTestPath())) {
+        if (!file_exists($this->getTestPath())) {
             File::makeDirectory($this->getTestPath());
+        }
+
+        if (!$this->option('without-abstracts')) {
+            $this->copyAbstractClasses();
         }
 
         $this->setDomainPath($this->getDomainPath(DIRECTORY_SEPARATOR . $this->getDirectory()));
         $this->setInfrastructurePath($this->getInfrastructurePath(DIRECTORY_SEPARATOR . $this->getDirectory()));
         $this->setTestPath($this->getTestPath(DIRECTORY_SEPARATOR . $this->getDirectory()));
 
-        if (!File::exists($this->getDomainPath())) {
+        if (!file_exists($this->getDomainPath())) {
             File::makeDirectory($this->getDomainPath());
             File::makeDirectory($this->getDomainPath('/Contracts'));
         }
 
-        if (!File::exists($this->getInfrastructurePath())) {
+        if (!file_exists($this->getInfrastructurePath())) {
             File::makeDirectory($this->getInfrastructurePath());
             File::makeDirectory($this->getInfrastructurePath('/Contracts'));
         }
 
-        if (!File::exists($this->getTestPath())) {
+        if (!file_exists($this->getTestPath())) {
             File::makeDirectory($this->getTestPath());
         }
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function copyAbstractClasses()
+    {
+        if (!file_exists("$this->infrastructurePath/Contracts/EloquentRepositoryInterface.php")) {
+            File::makeDirectory("$this->infrastructurePath/Contracts");
+
+            File::put(
+                "$this->infrastructurePath/Contracts/EloquentRepositoryInterface.php",
+                $this->prepare(File::get($this->abstractEloquentRepositoryInterface))
+            );
+        }
+
+        if (!file_exists("$this->infrastructurePath/EloquentRepositoryAbstract.php")) {
+            File::put(
+                "$this->infrastructurePath/EloquentRepositoryAbstract.php",
+                $this->prepare(File::get($this->abstractEloquentRepository))
+            );
+        }
+
+        if (!file_exists("$this->infrastructurePath/EloquentAbstract.php")) {
+            File::put(
+                "$this->infrastructurePath/EloquentAbstract.php",
+                $this->prepare(File::get($this->abstractEloquent))
+            );
+        }
+
+        if (!file_exists("$this->domainPath/RepositoryAbstract.php")) {
+            File::makeDirectory("$this->domainPath/Contracts");
+
+            File::put(
+                "$this->domainPath/Contracts/BaseEntityInterface.php",
+                $this->prepare(File::get($this->abstractBaseEntityInterface))
+            );
+        }
+
+        if (!file_exists("$this->domainPath/RepositoryAbstract.php")) {
+            File::put(
+                "$this->domainPath/RepositoryAbstract.php",
+                $this->prepare(File::get($this->abstractRepository))
+            );
+        }
+
+        if (!file_exists("$this->domainPath/ServiceAbstract.php")) {
+            File::put(
+                "$this->domainPath/ServiceAbstract.php",
+                $this->prepare(File::get($this->abstractService))
+            );
+        }
+
 
         return true;
     }
@@ -236,7 +310,12 @@ class GenerateDomain extends Command
 
     public function copyBinding($name)
     {
-        File::put($this->getDomainPath("/$name-binding.php"), $this->prepare(File::get($this->binding)));
+        $name = strtolower($name);
+        if (!file_exists(config_path('domains'))) {
+            File::makeDirectory(config_path('domains'));
+        }
+
+        File::put(config_path("domains/$name-binding.php"), $this->prepare(File::get($this->binding)));
         return true;
     }
 }
--- src/LaravelDddServiceProvider.php
+++ src/LaravelDddServiceProvider.php
@@ -23,10 +23,69 @@ class LaravelDddServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->config = $this->app->make('config');
+        $this->packages = $this->config->get('domains.user-binding');
+
+        $files = \File::allFiles(config_path('domains'));
+
+        foreach ($files as $file) {
+            if ($bindings = $this->config->get('domains.' . basename($file->getFilename(), '.php'))) {
+                if (array_has($bindings, 'providers')) {
+                    $this->provideService(array_get($bindings, 'providers'));
+                }
+
+                if (array_has($bindings, 'repositories')) {
+                    $this->provideRepositories(array_get($bindings, 'repositories'));
+                }
+
+                if (array_has($bindings, 'eloquent_repositories')) {
+                    $this->provideEloquentRepositories(array_get($bindings, 'eloquent_repositories'));
+                }
+            }
+        }
+
         if ($this->app->runningInConsole()) {
             $this->commands([
                 GenerateDomain::class,
             ]);
+        }
+    }
+
+    private function provideService(array $services)
+    {
+        foreach ($services as $key => $value) {
+            $this->app->bind($key, $value);
+        }
+    }
+
+    private function provideEloquentRepositories(array $eloquentRepositories)
+    {
+        foreach ($eloquentRepositories as $name => $eloquentRepository) {
+            $this->app->bind($name, function ($app) use ($eloquentRepository, $name) {
+
+                /**
+                 * Laravel 5.4 doesn't support $app->make with parameters. Uses contextual binding instead
+                 */
+                $this->app->when($eloquentRepository['class'])
+                    ->needs(\App\Infrastructures\EloquentAbstract::class)
+                    ->give($eloquentRepository['model']);
+
+                return $app->make($eloquentRepository['class']);
+            });
+        }
+    }
+
+
+    /**
+     * Provide repositories from packages
+     * @param array $repositories
+     *
+     * @return void
+     */
+    private function provideRepositories(array $repositories)
+    {
+        foreach ($repositories as $name => $provider) {
+            $this->app->bind($name, $provider);
         }
     }
 }
