5135757de63557f2f4f77e4c6d768ba05fb39d6f
olegfedoliak <oleg.fedoliak@gmail.com>
2017-08-25 22:45:51 +0300

    Added test

---
+++ tests/TraitTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace Test;
+
+use Mockery as m;
+use oleglfed\LaravelDDD\Traits\GeneratorTrait;
+use PHPUnit\Framework\TestCase;
+
+class TraitTest extends TestCase
+{
+    public $dummy;
+
+    /**
+     * Set up test.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->dummy = new Dummy();
+    }
+
+    public function testGetPath()
+    {
+        $this->dummy->setTestPath('test');
+        $this->assertEquals($this->dummy->getTestPath(), 'test');
+    }
+
+    public function testGetTable()
+    {
+        $this->dummy->table = 'test';
+        $this->assertEquals($this->dummy->getTable(), 'test');
+    }
+
+    public function testGetProvidedName()
+    {
+        $this->dummy->name = 'test';
+        $this->assertEquals($this->dummy->getProvidedName(), 'test');
+    }
+
+    public function testGetDirectory()
+    {
+        $this->dummy->directory = 'test';
+        $this->assertEquals($this->dummy->getDirectory(), 'test');
+    }
+}
+
+class Dummy {
+    use GeneratorTrait;
+}
